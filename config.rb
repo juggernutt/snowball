b94c3785fa4e173f2dc36a8efd38781631affd00
Erwin Groenewegen <erwingroenewegen87@gmail.com>
2014-09-24 21:22:10 +0200

    Added the Bootstrap FE-framework, on a SASS-based structure with an updated Ruby config. Also added an template folder with the default frame and some minor styling.

--- config.rb
+++ config.rb
@@ -2,11 +2,11 @@ require 'compass/import-once/activate'
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
-http_path = "public_html/"
-css_dir = "css"
+http_path = "/public_html"
+css_dir = "/public_html/css"
 sass_dir = "sass"
-images_dir = "img"
-javascripts_dir = "js"
+images_dir = "/public_html/img"
+javascripts_dir = "/public_html/js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
---
+++ public_html/templates/frame.tpl.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Matchfixing - The right way of fixing matches! The number one tournament generator</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link href="css/normalize.min.css" rel="stylesheet">
+
+		<link href="/css/screen.css" media="screen, projection" rel="stylesheet" type="text/css" />
+		<link href="/css/print.css" media="print" rel="stylesheet" type="text/css" />
+		<!--[if IE]>
+			<link href="/css/ie.css" media="screen, projection" rel="stylesheet" type="text/css" />
+		<![endif]-->
+
+        <script src="js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+    </head>
+    <body>
+
+		<nav class="header row navbar navbar-fixed-top">
+			<div class="col-xs-12 col-sm-5">
+				<a href="/">MATCHFIXING</a>
+			</div>
+			<div class="col-xs-12 col-sm-3 col-sm-offset-3 text-right">
+				<a href="#" class="btn btn-primary">Registreer</a>
+				<a href="#" class="btn btn-primary">Login</a>
+			</div>
+		</nav>
+		
+		<div class="jumbotron homepage-intro">
+			<div class="container">
+				<article class="text-center">
+					<h1>START FIXING YOUR OWN MATCHES</h1>
+					<p>
+						The fastes way of creating your own tournaments!
+					</p>
+					<a href="#" class="btn btn-primary">Create tournament</a>
+				</article>
+			</div>
+		</div>
+		
+		<div class="container">
+		</div>
+		
+        <p>Hello world! This is HTML5 Boilerplate.</p>
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.0.min.js"><\/script>')</script>
+
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+
+		<!-- GOOGLE ANALYTICS -->
+		
+		<!-- END GOOGLE ANALYTICS -->
+    </body>
+</html>
